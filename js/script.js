4c4e794dec90d51e6201caa72f8e92d815902a52
rizzle197 <connorgibson986@gmail.com>
2025-10-29 18:08:38 -0400

    created variables for the api link, if container, and button and updated the html to include the button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("script.js loaded");
+
+// Link to the giphy API with key and query "q"
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=fMhbYCtzDG99THAyTZBQnVoZoJXj2mbV&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+// Storing gif-container in a variable
+const gifContainer = document.querySelector("#gif-container");
+// Storing button in a variable
+const fetchButton = document.querySelector("#fetch-gif-btn");
