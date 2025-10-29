bfe1235bef0f32c5078cde0a3e1a19bcb879535e
rizzle197 <connorgibson986@gmail.com>
2025-10-29 18:31:37 -0400

    added a click event listener get the urls of the gifs from the search

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=fMhbYCtzDG99THAyT
 const gifContainer = document.querySelector("#gif-container");
 // Storing button in a variable
 const fetchButton = document.querySelector("#fetch-gif-btn");
+
+// Adding click event listener
+fetchButton.addEventListener("click", async function() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    // Looping through an array "images" and getting the url for each gif to display on my webpage
+    const images = [];
+    for (let i = 0; i < data.data.length; i++) {
+        const url = data.data[i].images.original.url;   // Getting the original url from current gif in the array
+        images.push(url);   //adding url to the array
+    }
+
+    console.log(images);     // Checking the urls in the array are correct through the console
+
+
+    
+});
